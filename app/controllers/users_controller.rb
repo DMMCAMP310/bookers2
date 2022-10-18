f54aa66e846f0580cbc54f060146f580b7f715a5
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-10-18 04:41:24 +0000

    [Finish]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+    @book_new = Book.new
+    @user = current_user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book_new = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(books_path) unless @user == current_user
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  devise_for :users
+  patch 'users/:id' => 'users#update', as: 'update_user'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  
+  get 'about' => 'homes#about'
+end
